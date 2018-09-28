e9b0e09770613952d3c4e407bcfa50e543535135
TheAllenAnker <theallenanker@gmail.com>
2018-09-28 08:25:56 +0800

    Adding login page and its serlvet controller.

--- src/com/allenanker/servlet/LoginServlet.java
+++ src/com/allenanker/servlet/LoginServlet.java
@@ -1,11 +1,15 @@
 package com.allenanker.servlet;
 
+import com.allenanker.bean.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/LoginServlet")
 public class LoginServlet extends HttpServlet {
@@ -14,6 +18,17 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        List<User> users = (List<User>) request.getServletContext().getAttribute("users");
+        for (User user : users) {
+            if (username.equals(user.getAccountName()) && password.equals(user.getPassword())) {
+                request.getSession().setAttribute("user", user);
+                response.sendRedirect(request.getContextPath() + "/success.jsp");
+                return;
+            }
+        }
+        request.setAttribute("msg", "Login failed. Account name or password is incorrect.");
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 }
---
+++ web/success.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Fenyr_Allen
+  Date: 2018-09-27
+  Time: 23:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login Successfully</title>
+</head>
+<body>
+<h1>You have successfully logined in.</h1>
+</body>
+</html>
